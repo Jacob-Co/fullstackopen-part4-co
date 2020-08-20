cc861424da77fbc1ce00c41e4c388f3f3e744e45
Jacobtco <jacobtco@gmail.com>
2020-08-20 11:44:54 +0800

    Seperated all /api/blogs routes into its own module

--- index.js
+++ index.js
@@ -9,27 +9,12 @@ require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const Blog = require('./models/blogs');
+const blogsRouter = require('./controllers/blogs');
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
