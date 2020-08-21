5cced4d19ae096ac2191dda47e4c2073a5d6d840
Jacobtco <jacobtco@gmail.com>
2020-08-22 07:56:36 +0800

    Added error middleware for api blog routes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 const mongoUrl = config.MONGODB_URI;
 console.log('Connecting to MongoDB...')
@@ -19,5 +21,6 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ utils/middleware.js
@@ -0,0 +1,17 @@
+const logger = require('./logger')
+
+const errorHandler = (e, req, res, next) => {
+  logger.error(e.message);
+
+  if (e.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id '});
+  }
+
+  if (e.name === 'ValidationError') {
+    return res.status(400).json({ error: e.message });
+  }
+
+  next(e);
+};
+
+module.exports = {errorHandler};
