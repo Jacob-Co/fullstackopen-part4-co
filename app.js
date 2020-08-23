b65c608e01fa14ce0d26ad7314b5c50a95bc5c08
Jacobtco <jacobtco@gmail.com>
2020-08-23 09:42:29 +0800

    Added new user model and user controller for POST new user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
@@ -22,6 +23,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
